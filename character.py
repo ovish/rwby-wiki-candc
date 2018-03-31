85f1d720db031ff307086a7a0ff39301bb1943d0
ovish <36478032+ovish@users.noreply.github.com>
2018-04-01 00:03:52 +0900

    cln:pyファイルの修正とj0の草刈

--- character.py
+++ character.py
@@ -10,59 +10,60 @@ with open('j0-all.md', 'r') as f:
     strJpn = f.read()
 
 # maketranseを使う
-result = strJpn.replace(
-    'Ruby Rose', 'ルビー・ローズ').replace(
-    'Weiss Schnee', 'ワイス・シュニー').replace(
-    'Blake Belladonna', 'ブレイク・ベラドンナ').replace(
-    'Yang Xiao Long', 'ヤン・シャオロン').replace(
+result =
+    strJpn.replace('Ruby Rose', 'ルビー・ローズ'
+    ).replace('Weiss Schnee', 'ワイス・シュニー'
+    ).replace('Blake Belladonna', 'ブレイク・ベラドンナ'
+    ).replace('Yang Xiao Long', 'ヤン・シャオロン'
 
-    'Jaune Arc', 'ジョーン・アーク').replace(
-    'Nora Valkyrie', 'ノーラ・ヴァルキリー').replace(
-    'Pyrrha Nikos', 'ピュラ・ニコス').replace(
-    'Lie Ren', 'ライ・レン').replace(
+    ).replace('Jaune Arc', 'ジョーン・アーク'
+    ).replace('Nora Valkyrie', 'ノーラ・ヴァルキリー'
+    ).replace('Pyrrha Nikos', 'ピュラ・ニコス'
+    ).replace('Lie Ren', 'ライ・レン'
 
-    'Ozpin', 'オズピン').replace(
-    'Glynda Goodwitch', 'グリンダ・グッドウィッチ').replace(
-    'Peter Port', 'ピーター・ポート').replace(
-    'Bartholomew Oobleck', 'バーソロミュー・ウーブレック').replace(
+    ).replace('Ozpin', 'オズピン'
+    ).replace('Glynda Goodwitch', 'グリンダ・グッドウィッチ'
+    ).replace('Peter Port', 'ピーター・ポート'
+    ).replace('Bartholomew Oobleck', 'バーソロミュー・ウーブレック'
 
-    'James Ironwood', 'ジェームズ・アイアンウッド').replace(
-    'Winter Schnee', 'ウィンター・シュニー').replace(
-    'Penny Polendina', 'ペニー・ポレンディーナ').replace(
+    ).replace('James Ironwood', 'ジェームズ・アイアンウッド'
+    ).replace('Winter Schnee', 'ウィンター・シュニー'
+    ).replace('Penny Polendina', 'ペニー・ポレンディーナ'
 
-    'Summer Rose', 'サマー・ローズ').replace(
-    'Taiyang Xiao Long', 'タイヤン・シャオロン').replace(
-    'Raven Branwen', 'レイヴン・ブランウェン').replace(
-    'Qrow Branwen', 'クロウ・ブランウェン').replace(
+    ).replace('Summer Rose', 'サマー・ローズ'
+    ).replace('Taiyang Xiao Long', 'タイヤン・シャオロン'
+    ).replace('Raven Branwen', 'レイヴン・ブランウェン'
+    ).replace('Qrow Branwen', 'クロウ・ブランウェン'
 
-    'Salem', 'セイラム').replace(
-    'Arthur Watts', 'アーサー・ワッツ').replace(
-    'Hazel Rainart', 'ヘイゼル・レイナート').replace(
-    'Tyrian Callows', 'ティリアン・キャロウズ').replace(
+    ).replace('Salem', 'セイラム'
+    ).replace('Arthur Watts', 'アーサー・ワッツ'
+    ).replace('Hazel Rainart', 'ヘイゼル・レイナート'
+    ).replace('Tyrian Callows', 'ティリアン・キャロウズ'
 
-    'Cinder Fall', 'シンダー・フォール').replace(
-    'Roman Torchwick', 'ローマン・トーチウィック').replace(
-    'Emerald Sustrai', 'エメラルド・サストライ').replace(
-    'Mercury Black', 'マーキュリー・ブラック').replace(
-    'Neopolitan', 'ニオポリタン').replace(
+    ).replace('Cinder Fall', 'シンダー・フォール'
+    ).replace('Roman Torchwick', 'ローマン・トーチウィック'
+    ).replace('Emerald Sustrai', 'エメラルド・サストライ'
+    ).replace('Mercury Black', 'マーキュリー・ブラック'
+    ).replace('Neopolitan', 'ニオポリタン'
 
-    'Adam Taurus', 'アダム・トーラス').replace(
-    'White Fang Lieutenant', 'ホワイト・ファングの中尉').replace(
+    ).replace('Adam Taurus', 'アダム・トーラス'
+    ).replace('White Fang Lieutenant', 'ホワイト・ファングの中尉'
 
-    'Cardin Winchester', 'カーディン・ウィンチェスター').replace(
-    'Russel Thrush', 'ラッセル・スラッシュ').replace(
-    'Dove Bronzewing', 'ダヴ・ブロンズウィング').replace(
-    'Sky Lark', 'スカイ・ラーク').replace(
+    ).replace('Cardin Winchester', 'カーディン・ウィンチェスター'
+    ).replace('Russel Thrush', 'ラッセル・スラッシュ'
+    ).replace('Dove Bronzewing', 'ダヴ・ブロンズウィング'
+    ).replace('Sky Lark', 'スカイ・ラーク'
 
-    'Coco Adel', 'ココ・アデル').replace(
-    'Fox Alistair', 'フォックス・アリステア').replace(
-    'Velvet Scarlatina', 'ヴェルヴェット・スカーラティーナ').replace(
-    'Yatsuhashi Daichi', 'ヤツハシ・ダイチ').replace(
+    ).replace('Coco Adel', 'ココ・アデル'
+    ).replace('Fox Alistair', 'フォックス・アリステア'
+    ).replace('Velvet Scarlatina', 'ヴェルヴェット・スカーラティーナ'
+    ).replace('Yatsuhashi Daichi', 'ヤツハシ・ダイチ'
 
-    'Sun Wukong', 'サン・ウーコン').replace(
-    'Scarlet David', 'スカーレット・デイビッド').replace(
-    'Sage Ayana', 'セージ・アヤナ').replace(
-    'Neptune Vasilias', 'ネプチューン・ヴァシリアス')
+    ).replace('Sun Wukong', 'サン・ウーコン'
+    ).replace('Scarlet David', 'スカーレット・デイビッド'
+    ).replace('Sage Ayana', 'セージ・アヤナ'
+    ).replace('Neptune Vasilias', 'ネプチューン・ヴァシリアス'
+    )
 
 
 with open('result.md', 'w') as f:
